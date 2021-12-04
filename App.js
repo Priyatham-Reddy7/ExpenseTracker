d2d53d7a68c6597f4397daba36b2543604bd9c3f
Priyatham-Reddy7 <87036611+Priyatham-Reddy7@users.noreply.github.com>
2021-12-04 12:22:32 +0530

    Files added

---
+++ App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Header';
+import Balance from './Balance';
+import IncomeExpenses from './IncomeExpenses';
+import TransactionList from './TransactionList';
+import AddTransaction from './AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider >
+      <Header />
+      <div className="conatiner">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
---
+++ context/GlobalState.js
@@ -0,0 +1,48 @@
+import React, {createContext, useContext, useReducer} from 'react';
+import AppReducer from './AppReducer'
+
+//? creating Initial State
+
+const initialState = {
+    transactions: [
+            { id: 1, text: 'Salary', amount: 30000 },
+            { id: 2, text: 'EMI', amount: -10000 },
+            { id: 3, text: 'Rent', amount: +6500 },
+            { id: 4, text: 'Groceries', amount: -2000 }
+    ]
+}
+
+//?creating global context
+export const GlobalContext = createContext(initialState);
+
+//?provider ELement
+export const GlobalProvider = ({ children })=>{
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    //?Actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+        
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+        
+    }
+
+    return(
+        <GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction,
+        addTransaction
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+
